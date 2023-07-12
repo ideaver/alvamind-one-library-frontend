5766ae94700d012881d4da784c60a3b0b0e868a7
dsaauser4 <Dsaauser4@gmail.com>
2023-07-12 10:12:02 +0700

    update organism

--- lib/view/samples/service_category_samples_view.dart
+++ lib/view/samples/service_category_samples_view.dart
@@ -1,11 +1,8 @@
-import 'package:alvamind_library/app/asset/app_assets.dart';
 import 'package:alvamind_library/view/samples/sample_wrapper.dart';
 import 'package:flutter/material.dart';
 
-import '../../app/theme/app_colors.dart';
 import '../../app/theme/app_sizes.dart';
-import '../../model/category_menu_model.dart';
-import '../../widget/molecule/app_category_menu.dart';
+
 import '../../widget/organism/service_category_menu/service_category_menu.dart';
 
 class ServiceCategorySamplesView extends StatefulWidget {
