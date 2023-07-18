d43d36efe2fa773e4d134da922b5f40a3b9fdfe5
dsaauser4 <Dsaauser4@gmail.com>
2023-07-18 20:57:53 +0700

    added and updated organsim

--- lib/view/samples/bar_chart_samples_view.dart
+++ lib/view/samples/bar_chart_samples_view.dart
@@ -1,7 +1,11 @@
+import 'package:alvamind_library/app/theme/app_text_style.dart';
+import 'package:alvamind_library/widget/molecule/app_icon_button.dart';
+import 'package:alvamind_library/widget/molecule/app_long_card.dart';
 import 'package:flutter/material.dart';
 import '../../app/theme/app_colors.dart';
 import '../../app/theme/app_sizes.dart';
 
+import '../../widget/organism/tags_organism.dart/tags_organism.dart';
 import 'sample_wrapper.dart';
 
 class BarChartSamplesView extends StatefulWidget {
@@ -22,7 +26,64 @@ class _BarChartSamplesViewState extends State<BarChartSamplesView> {
         padding: EdgeInsets.all(AppSizes.padding),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [],
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AppLongCard(
+                  padding: EdgeInsets.all(0),
+                  children: [
+                    Row(
+                      children: [
+                        AppIconButton(
+                            buttonColor: AppColors.blueLv5,
+                            icon: Icon(
+                              Icons.insert_chart_outlined_rounded,
+                              color: AppColors.primary,
+                            ),
+                            onTap: () {}),
+                        SizedBox(
+                          width: AppSizes.padding,
+                        ),
+                        Text(
+                          'Omzet',
+                          style: AppTextStyle.bold(size: 24),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: AppSizes.padding,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('1 Jan 2023 - 31 Juli 2023', style: AppTextStyle.regular(size: 14, color: AppColors.blackLv4)),
+                    SizedBox(height: AppSizes.padding / 2),
+                    Text('Rp 687.375.337', style: AppTextStyle.bold(size: 24)),
+                    SizedBox(height: AppSizes.padding / 2),
+                    Text('1.000 Transaksi', style: AppTextStyle.regular(size: 14, color: AppColors.blackLv4)),
+                  ],
+                ),
+                SizedBox(
+                  height: AppSizes.padding,
+                ),
+                TagsOrganism(
+                  listChips: const [
+                    'Drop Service',
+                    'Self Service',
+                    'Delivery',
+                    'Home Service',
+                  ],
+                  selected: -1,
+                  onTap: (value) {
+                    // TODO
+                  },
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
