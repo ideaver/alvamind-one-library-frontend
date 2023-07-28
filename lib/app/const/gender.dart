b19fb21fd1ecc64889a9313a1f0ea890cebfa062
dsaauser4 <Dsaauser4@gmail.com>
2023-07-28 13:45:12 +0700

    updated

---
+++ lib/app/const/gender.dart
@@ -0,0 +1,12 @@
+import 'package:alvamind_library/model/gender_model.dart';
+
+List<GenderModel> gender = const [
+  GenderModel(
+    textGender: 'Lelaki',
+    codeGender: 'L',
+  ),
+  GenderModel(
+    textGender: 'Perempuan',
+    codeGender: 'P',
+  ),
+];
---
+++ lib/app/const/problem.dart
@@ -0,0 +1,20 @@
+import 'package:alvamind_library/model/complain_model.dart';
+
+List<ComplainModel> solution = const [
+  ComplainModel(
+    textComplain: 'Kehilangan',
+    codeComplain: '011',
+  ),
+  ComplainModel(
+    textComplain: 'Noda',
+    codeComplain: '012',
+  ),
+  ComplainModel(
+    textComplain: 'Kerusakan',
+    codeComplain: '013',
+  ),
+  ComplainModel(
+    textComplain: 'Benda Asing',
+    codeComplain: '014',
+  ),
+];
